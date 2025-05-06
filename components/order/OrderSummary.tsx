dcd5256d9f260f6a83c26b17be6d6abbc2bcd97a
EliDev <oscarmateoelias@gmail.com>
2025-05-05 23:06:21 -0600

    WIP: Migrate Order and Products relationship; create first action to get and save name and order details

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -3,19 +3,26 @@ import { useStore } from "@/src/store";
 import ProductDetails from "./ProductDetails";
 import { useMemo } from "react";
 import { formatCurrency } from "@/src/utils";
+import { createOrder } from "@/actions/create-order-action";
 
 export default function OrderSummary() {
   const order = useStore((state) => state.order);
-  
+
   const total = useMemo(
     () =>
       // Use reduce to iterate over the order array and calculate the total
       order.reduce(
-        (total, item) => total + (item.quantity * item.price),   // Add the product of quantity and price of each item to the total
+        (total, item) => total + item.quantity * item.price, // Add the product of quantity and price of each item to the total
         0 // Initial value of the total is 0
       ),
     [order] // Recalculate only when the order array changes
   );
+
+  const handleCreateOrder = (formData: FormData) => {
+    console.log(formData.get('name'));
+    
+    createOrder();
+  };
 
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
@@ -23,7 +30,7 @@ export default function OrderSummary() {
 
       {order.length === 0 ? (
         <p className="text-gray-500 text-xl font-bold text-center mt-5">
-          The shop car is empty
+          The Order is empty
         </p>
       ) : (
         <div className="mt-5">
@@ -35,6 +42,21 @@ export default function OrderSummary() {
             Total amount: {""}
             <span className="font-bold">{formatCurrency(total)}</span>
           </p>
+
+          <form className="w-full mt-10 space-y-5" action={handleCreateOrder}>
+            <input
+              type="text"
+              placeholder="Your name"
+              className="bg-white border border-gray-100 w-full p-2"
+              name="name"
+            />
+
+            <input
+              type="submit"
+              className="font-bold py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer"
+              value="Confirm Order"
+            />
+          </form>
         </div>
       )}
     </aside>
